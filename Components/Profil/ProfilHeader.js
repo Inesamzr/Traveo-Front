8f147c3fa8aa79c840667a7173299f3821468451
Amel <ameladdou213@gmail.com>
2024-11-25 14:34:50 +0100

    adding logout from profil

--- Components/Profil/ProfilHeader.js
+++ Components/Profil/ProfilHeader.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { logout } from '../../services/authService';
+import { useNavigation } from '@react-navigation/native';
 
-export default function ProfilHeader({ name, onEdit }) {
+export default function ProfilHeader({ name }) {
+  const navigation = useNavigation();
+
+  const handleLogout = async () => {
+    try {
+      await logout(); // Supprimez les données utilisateur du stockage
+      Alert.alert('Déconnexion réussie', 'Vous avez été déconnecté.');
+      navigation.navigate('Login'); // Redirigez vers la page de connexion
+    } catch (error) {
+      Alert.alert('Erreur', "Une erreur est survenue lors de la déconnexion.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.avatarContainer} onPress={onEdit}>
+      <TouchableOpacity onPress={handleLogout} style={styles.backIcon}>
+        <MaterialIcons name="logout" size={24} color="#510D0A" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.avatarContainer}>
         <Image
           source={require('../../assets/avatar.png')} // Remplacez par votre image d'avatar
           style={styles.avatar}
@@ -33,22 +51,18 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     backgroundColor: '#A5C49F',
   },
-  editIcon: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 4,
-  },
-  icon: {
-    width: 15,
-    height: 15,
-  },
   name: {
     marginTop: 10,
     fontSize: 20,
     fontWeight: '600',
     color: '#386641',
+  },
+  backIcon: {
+    position: 'absolute',
+    top: 40,
+    right: 20,
+    padding: 8,
+    borderRadius: 20,
+    zIndex: 2,
   },
 });
