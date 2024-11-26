045701d76538742d1e6e0b789548cdd13c739e90
Amel <ameladdou213@gmail.com>
2024-11-26 22:40:29 +0100

    adding addReview method in services

--- services/reviewService.js
+++ services/reviewService.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const API_URL = 'http://10.193.2.198:8088/api/avis/';
+const API_URL = 'http://10.193.2.198:8088/api/avis';
 
 export const fetchReviewsByReservationId = async (reservationId) => {
-    console.log(`${API_URL}activite/${reservationId}`)
-  const response = await axios.get(`${API_URL}activite/${reservationId}`);
+  const response = await axios.get(`${API_URL}/activite/${reservationId}`);
+  return response.data; 
+};
+
+export const addReview = async (data) => {
+    console.log(data)
+  const response = await axios.post(`${API_URL}`, data);
   return response.data; 
 };
