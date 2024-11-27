721195f0ee835fb4d636d85dd1fac5af3190ac84
Amel <ameladdou213@gmail.com>
2024-11-27 23:17:44 +0100

    not showing personal crendiatials

--- services/userService.js
+++ services/userService.js
@@ -2,10 +2,10 @@
 import axios from 'axios';
 import apiClient from './apiClient';
 
-//const USER_API_URL = 'http://10.193.2.198:8080/api/user';
+const USER_API_URL = 'http://10.193.2.198:8080/api/users';
 //const USER_API_URL = 'http://192.168.1.54:8080/api/user';
 //const USER_API_URL = 'http://162.38.32.231:8080/api/user';
-const USER_API_URL = 'http://162.38.37.37:8080/api/user';
+//const USER_API_URL = 'http://162.38.37.37:8080/api/user';
 
 export const getUserById = async (userId) => {
   const response = await axios.get(`${USER_API_URL}/${userId}`);
