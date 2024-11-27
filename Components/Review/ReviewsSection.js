eccf241ba310517e5d2af6b6c4797f4d24a205b1
Amel <ameladdou213@gmail.com>
2024-11-27 09:55:38 +0100

    adding delete and update review in section

--- Components/Review/ReviewsSection.js
+++ Components/Review/ReviewsSection.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
 import ReviewSummary from './ReviewSummary';
 import ReviewCard from './ReviewCard';
+import { deleteReview } from '../../services/reviewService';
+import { useNavigation } from '@react-navigation/native';
 
-export default function ReviewsSection({ reviews, rating, reviewsCount }) {
+export default function ReviewsSection({ reviews, rating, reviewsCount}) {
   const [visibleReviews, setVisibleReviews] = useState(2); // Nombre d'avis visibles
+
+  const navigation = useNavigation()
 
   const handleSeeMore = () => {
     if (visibleReviews < reviews.length) {
@@ -14,18 +18,59 @@ export default function ReviewsSection({ reviews, rating, reviewsCount }) {
     }
   };
 
+  const handleDelete = async (reviewId) => {
+    Alert.alert(
+      "Supprimer l'avis",
+      "Êtes-vous sûr de vouloir supprimer cet avis ?",
+      [
+        {
+          text: "Annuler",
+          style: "cancel",
+        },
+        {
+          text: "Supprimer",
+          onPress: async () => {
+            try {
+              await deleteReview(reviewId);
+              Alert.alert("Succès", "L'avis a été supprimé avec succès.");
+              // Supprimer l'avis localement
+              setVisibleReviews(visibleReviews - 1);
+            } catch (error) {
+              Alert.alert("Erreur", "Impossible de supprimer l'avis. Réessayez plus tard.");
+              console.error(error);
+            }
+          },
+        },
+      ]
+    );
+  };
+
+  const handleUpdate = (reviewId) => {
+    const reviewToEdit = reviews.find((review) => review.idAvis === reviewId);
+    if (reviewToEdit) {
+      navigation.navigate('EditReview', { review: reviewToEdit });
+    } else {
+      console.error('Avis non trouvé pour l\'ID:', reviewId);
+    }
+  };
+  
+
   return (
     <View style={styles.container}>
       <ReviewSummary rating={rating} reviewsCount={reviewsCount} />
-      {reviews && reviews.slice(0, visibleReviews).map((review, index) => (
-        <ReviewCard
-          key={index}
-          userId={review.userId}
-          rating={review.note}
-          comment={review.commentaire}
-          avatarColor={index % 2 === 0 ? '#DDE6C7' : '#F5D4DB'}
-        />
-      ))}
+      {reviews &&
+        reviews.slice(0, visibleReviews).map((review, index) => (
+          <ReviewCard
+            key={index}
+            id={review.idAvis}
+            userId={review.userId}
+            rating={review.note}
+            comment={review.commentaire}
+            avatarColor={index % 2 === 0 ? '#DDE6C7' : '#F5D4DB'}
+            onDelete={handleDelete}
+            onEdit={handleUpdate}
+          />
+        ))}
       {reviews && visibleReviews < reviews.length && (
         <TouchableOpacity style={styles.button} onPress={handleSeeMore}>
           <Text style={styles.buttonText}>Voir plus d'avis</Text>
@@ -38,7 +83,7 @@ export default function ReviewsSection({ reviews, rating, reviewsCount }) {
 const styles = StyleSheet.create({
   container: {
     marginTop: 30,
-    marginBottom: 80
+    marginBottom: 80,
   },
   button: {
     backgroundColor: '#D9A5B3',
