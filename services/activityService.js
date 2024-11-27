9787cd9f6d4743a23e7f63488464f2a3033d2656
inesamzr <ines.amzert@etu.umontpellier.fr>
2024-11-27 16:03:17 +0100

    fonctionnalité ajout activité

--- services/activityService.js
+++ services/activityService.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 //const ACTIVITY_API_URL = 'http://10.193.2.198:8086/api/activities';
-//const ACTIVITY_API_URL = 'http://162.38.37.37:8086/api/activities';
 const ACTIVITY_API_URL = 'http://162.38.37.37:8086/api/activities';
 //const ACTIVITY_API_URL = 'http://192.168.1.54:8086/api/activities';
-
 
 
 // Récupérer toutes les activités
