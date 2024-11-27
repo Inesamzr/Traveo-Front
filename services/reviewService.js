135a5bc8427d0d3567e16518ae5defd2359d82a2
Amel <ameladdou213@gmail.com>
2024-11-27 09:34:35 +0100

    adding delete reviews func in service

--- services/reviewService.js
+++ services/reviewService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://10.193.2.198:8088/api/avis';
+const API_URL = 'http://162.38.32.231:8088/api/avis';
 
 export const fetchReviewsByReservationId = async (reservationId) => {
   const response = await axios.get(`${API_URL}/activite/${reservationId}`);
@@ -12,3 +12,10 @@ export const addReview = async (data) => {
   const response = await axios.post(`${API_URL}`, data);
   return response.data; 
 };
+
+
+export const deleteReview = async (reviewId) => {
+  const response = await axios.delete(`${API_URL}/${reviewId}`);
+  return response.data;
+};
+
